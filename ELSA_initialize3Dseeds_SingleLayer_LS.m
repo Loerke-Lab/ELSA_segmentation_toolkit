2a273dfd8ef6c542fbf71e011a45a56cb7df811b
katiembond <katiebondak@gmail.com>
2022-09-08 21:25:01 -0600

    Added comments to clarify the code

--- ELSA_initialize3Dseeds_SingleLayer_LS.m
+++ ELSA_initialize3Dseeds_SingleLayer_LS.m
@@ -1,24 +1,40 @@
 function []= ELSA_initialize3Dseeds_SingleLayer_LS(data,t,layer,seedfilter)
 %ELSA_initialize3Dseeds_SingleLayer_LS allows the user to initialize the
 %seeds and mask for a single layer for 3D image data.
+%
+% INPUT:    data: structure that contains the fields 'ImageFileList' and
+%                 'Source'.
+%           t: the time frame to initialize the seeds
+%           layer: the z layer to initialize the seeds (if movie is only 2D
+%                  this will be 1)
+%           seedfilter: Gaussian filter size. Must have the function
+%                       "filterImage3DpaddedEdges.m"
+%
+% OUTPUT:   No output but the results are saved to the SegmentationData
+%           folder associated with the input time frame
  
 % get original directory
 od = cd;
- 
+
+% get information from data structure
 list = data.ImageFileList;
 pathimages = data.Source; 
+
+% move to the image path
 cd(pathimages);
+
+% get image data to use for initializing seeds
 Ifull = tiffreadVolume(list{t});
 for z = 1:size(list,2)
     Ifull(:,:,z) = imread(list{t,z});
 end
 
-
+% move to frame specific segmentation folder
 cd('SegmentationData');
 cframefoldername = sprintf('frame%04d',t);
 cd(cframefoldername);
-% load seeds and mask
 
+% load seeds, mask, ImageBWlabel, and ImageSegment
 seeds = load('seeds.mat').seeds;
 mask = load('mask.mat').mask;
 ImageBWlabel = load('ImageBWlabel.mat').ImageBWlabel;
@@ -32,20 +48,25 @@ ImageSegment = load('ImageSegment.mat').ImageSegment;
 % ImBoundarySeed(:,end) = true;
 % ImBoundarySeed(end,:) = true;
 
+% filter the image
 Ifull_Filt = imgaussfilt3(Ifull,[2 2 1]);
 
+% get number of layers
 Nlayers = size(Ifull,3);
 
 
-
-    
+% get specific layer for image and filtered image specified by layer input   
 Ilayer= Ifull(:,:,layer);
 Ilayer_Filt= Ifull_Filt(:,:,layer);
+
+% create title string to display during seed initialization
 titlestr = strcat('Layer: ',num2str(layer),', Depth: ',num2str((layer-1)*0.5));
+
+% initialize the seeds and mask
 [seeds(:,:,layer),mask(:,:,layer),ImageBWlabel(:,:,layer),ImageSegment(:,:,layer)] = modifySeedsAndMask_LS(Ilayer,Ilayer_Filt,seeds(:,:,layer),mask(:,:,layer),titlestr,seedfilter);
     
 
-    
+% save seeds, mask, ImageBWlabel, and ImageSegment   
 save('ImageSegment', 'ImageSegment');
 save('ImageBWlabel', 'ImageBWlabel');
 save('seeds','seeds');
@@ -54,21 +75,47 @@ save('mask','mask');
 % return to original directory
 cd(od)
  
- 
+% close figure 
 close(gcf)
+
 end
 
 
  
 function [seeds,mask,imageBWlabel,imageSegmented] = modifySeedsAndMask_LS(I,Ifilt,seeds,mask,titlestr,seedfilter)
+% this function allows the user to make easy changes to watershed seeds and
+% background mask
+%
+% INPUT:    I:  The image to display while modifications are made with RGB
+%               values
+%           Ifilt:  The filtered image used for segmentation
+%           seeds:  Logical matrix of seed regions for cells
+%           mask:   Logical matrix of background mask regions
+%           titlestr: The title to diplayed on the figure
+%           seedfilter: Gaussian filter size. Must have the function
+%                       "filterImage3DpaddedEdges.m"
+%
+% OUTPUT:   seeds: Logical matrix of new seed regions for cells
+%           mask: Logical matrix of new background mask regions
+%           imageBWlabel: An integer labeled image where cells have unique
+%                         integer labels, interfaces are zeros, and background is -1
+%           imageSegmented: Logical matrix where segmented regions are true and
+%                           boundary lines (interfaces) are false
 
+
+
+
+% set structuring elements
 SEpoints = strel('disk',7);
 SEmorph  = strel('disk',3);
 SEseg    = strel('disk',1);
 SEcloseMask   = strel('disk',10);
 SEerodeMask = strel('disk',10);
 
+% set magnification value
 magVal = 200;
+
+% set exit flag to false
 eflag = false;
  
 % rescale image (if not already the case)
@@ -79,8 +126,10 @@ I = imadjust(I);
 % initialize the modified seeds and mask to the input seeds and mask
 seg = false(size(seeds));
  
-
+% set unsatisfactory to true
 unsatisfactory = true;
+
+% continue to loop while unsatisfactory is true
 while unsatisfactory
 
     %seeds = bwareaopen(seeds,5);
@@ -115,10 +164,13 @@ while unsatisfactory
     % hold current seeds and mask in case changes need to be undone
     
     switch choice
-        case 1 % Continue
+
+        % Continue
+        case 1
             unsatisfactory = false;
-            
-        case 2 % Mask to Seed
+        
+        % Mask to Seed    
+        case 2
             last_seeds = seeds;
             last_mask = mask;
             
@@ -162,8 +214,8 @@ while unsatisfactory
             end
             %bwmorph(seeds,'shrink',10);
             
-            
-        case 3 % convert Seed to Mask
+        % convert Seed to Mask    
+        case 3
             last_seeds = seeds;
             last_mask = mask;
             
@@ -183,8 +235,8 @@ while unsatisfactory
             mask = imclose(BW | mask,SEcloseMask);
             mask = imerode(mask,SEerodeMask);
 
-            
-        case 4 % Add Polygon Seed
+        % Add Polygon Seed    
+        case 4
             last_seeds = seeds;
             last_mask = mask;
             
@@ -194,7 +246,8 @@ while unsatisfactory
             seeds = bwmorph(seeds,'shrink',10);
             mask(imdilate(BW,SEerodeMask)) = false;
             
-        case 5 % Add Polygon Mask 
+        % Add Polygon Mask    
+        case 5 
             last_seeds = seeds;
             last_mask = mask;
             
@@ -202,8 +255,9 @@ while unsatisfactory
             title(gca,'Add Polygon Mask','FontSize',16);                
             mask = mask | BW; 
             seeds(imdilate(BW,SEerodeMask)) = false;
-            
-        case 6 % Remove Polygon (removes polygon from both seeds and mask)
+        
+        %  Remove Polygon (removes polygon from both seeds and mask)    
+        case 6
             last_seeds = seeds;
             last_mask = mask;
             
@@ -211,8 +265,9 @@ while unsatisfactory
             title(gca,'Remove Polygon','FontSize',16);                
             seeds(BW) = false;
             mask(BW) = false;            
-            
-        case 7 % Add Point Seeds (can continue adding until user hits enter).
+        
+        % Add Point Seeds (can continue adding until user hits enter)    
+        case 7
             last_seeds = seeds;
             last_mask = mask;
             
@@ -227,8 +282,8 @@ while unsatisfactory
             BW = imdilate(BW,SEpoints);
             seeds = seeds | BW;
             
-
-        case 8 % Re-initialize
+        % Re-initialize
+        case 8
             last_seeds = seeds;
             last_mask = mask;
             
@@ -273,11 +328,14 @@ while unsatisfactory
             for ii=1:numel(idxWeakRatio)
                 seeds(Lseeds==idxWeakRatio(ii)) = 0;
             end
-            
-        case 9 % Undo last change
+        
+        % Undo last change
+        case 9
             seeds = last_seeds;
             mask  = last_mask;
-        case 10 % Exit Function
+
+        % Exit Function    
+        case 10
             eflag = true;
             return
 
@@ -285,13 +343,15 @@ while unsatisfactory
  
     end % switch
 
+
+    % after modifications have been made use the new seeds and mask to
+    % generate a new segmentation and new "shrunken" seeds
     imageMasked = imimposemin(Ifilt, seeds|mask);
     L = watershed(imageMasked,8);
     imageSegmented = L;
     imageSegmented(imageSegmented>0) = 1;
     imageSegmented = logical(imageSegmented);
     seg = imdilate(L==0,SEseg);
-
 
 
     maskLabels = unique(L(mask));
@@ -342,8 +402,9 @@ while unsatisfactory
 end
  
  
- 
+% close everything 
 close all
+
 end
 
 
