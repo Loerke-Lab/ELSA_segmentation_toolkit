117aede604891e04e42d8e7a2854daaa3fcd1e72
katiembond <katiebondak@gmail.com>
2022-10-27 09:55:41 -0600

    Update MakeFileList_LS with additional comments

--- MakeFileList_LS.m
+++ MakeFileList_LS.m
@@ -1,10 +1,20 @@
 function [ListCh0,ListCh1] = MakeFileList_LS(path) %,List2
 % This function generates an image file list cell array from the TIF files
 % in the directory 'path' or in the current directory (default).
+%
+% INPUT:    path: (optional) directory path where to look for images, if 
+%                  this is not specified, the function uses the current
+%                  directory by default
+%
+% OUTPUT:   ListCh0: file list of image channel one
+%           ListCh1: file list of image channel two
 
+
+% check if path was inputted and change directory to that path
 if nargin == 1
     cd(path)
 end
+
 
 Mylist = dir('*.tif');
 
@@ -16,20 +26,24 @@ idxListCh0 = 1;
 FrameVecCh0 = [];
 idxListCh1 = 1;
 FrameVecCh1 = [];
+
+
 for ii=1:Nfiles
     NamePartsCells = strsplit(MylistNames{ii},'_');
-    Channel = NamePartsCells{4}; % Use 4 for 2colorWT, Use index 3 for Y27, Use index 4 for zip, Use index 4 for nls
-    Frame   = NamePartsCells{6}; % Use 6 for 2colorWT, Use index 5 for Y27, Use index 6 for zip, Use index 6 for nls
+    Channel = NamePartsCells{3}; % Use 4 for 2colorWT, Use index 3 for Y27, Use index 4 for zip, Use index 4 for nls
+    Frame   = NamePartsCells{2}; % Use 6 for 2colorWT, Use index 5 for Y27, Use index 6 for zip, Use index 6 for nls
     
     Frame(1:5) = [];
     FrameInt = str2num(Frame);
     
-    if strcmpi(Channel,'ch0')
+    %if strcmpi(Channel,'ch0')
+    if strcmpi(Channel,'GFP')    
         ListCh0{idxListCh0} = MylistNames{ii};
         idxListCh0 = idxListCh0 + 1;
         FrameVecCh0 = [FrameVecCh0;FrameInt];
     end
-    if strcmpi(Channel,'ch1')
+    %if strcmpi(Channel,'ch1')
+    if strcmpi(Channel,'mCherry')    
         ListCh1{idxListCh1} = MylistNames{ii};
         idxListCh1 = idxListCh1 + 1;
         FrameVecCh1 = [FrameVecCh1;FrameInt];
